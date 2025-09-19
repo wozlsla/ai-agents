0ec8e7f30b7d0c9fe683329c4d1089aeaba75f6f
jimin <jiminseo0307@gmail.com>
2025-09-19 20:25:30 +0900

    feat(content-pipeline-agent): 5.3 refinement loop

--- content-pipeline-agent/main.py
+++ content-pipeline-agent/main.py
@@ -12,6 +12,12 @@ class ContentPipelineState(BaseModel):
 
     # Internal
     max_length: int = 0
+    score: int = 0
+
+    # Content
+    blog_post: str = ""
+    tweet: str = ""
+    linkedin_post: str = ""
 
 
 class ContetntPipelineFlow(Flow[ContentPipelineState]):
@@ -40,7 +46,7 @@ class ContetntPipelineFlow(Flow[ContentPipelineState]):
         return True
 
     @router(conduct_research)
-    def router(self):
+    def conduct_research_router(self):
 
         content_type = self.state.content_type
 
@@ -51,16 +57,22 @@ class ContetntPipelineFlow(Flow[ContentPipelineState]):
         else:
             return "make_linkedin_post"
 
-    @listen("make_blog")
+    @listen(or_("make_blog", "remake_blog"))
     def handle_make_blog(self):
+        # if blog post has been made, show the old one to the ai and ask it to improve, else
+        # just ask to create.
         print("Making blog post...")
 
-    @listen("make_tweet")
+    @listen(or_("make_tweet", "remake_tweet"))
     def handle_make_tweet(self):
+        # if tweet has been made, show the old one to the ai and ask it to improve, else
+        # just ask to create.
         print("Making tweet post...")
 
-    @listen("make_linkedin_post")
+    @listen(or_("make_linkedin_post", "remake_linkedin_post"))
     def handle_make_linkedin_post(self):
+        # if post has been made, show the old one to the ai and ask it to improve, else
+        # just ask to create.
         print("Making linkedin post...")
 
     @listen(handle_make_blog)
@@ -71,7 +83,22 @@ class ContetntPipelineFlow(Flow[ContentPipelineState]):
     def check_virality(self):
         print("Checking virality...")
 
-    @listen(or_(check_seo, check_virality))
+    @router(or_(check_seo, check_virality))
+    def score_router(self):
+        content_type = self.state.content_type
+        score = self.state.score
+
+        if score >= 8:
+            return "check_passed"
+        else:
+            if content_type == "blog":
+                return "remake_blog"
+            elif content_type == "linkedin":
+                return "remake_linkedin_post"
+            else:
+                return "remake_tweet"
+
+    @listen("check_passed")
     def finalize_content(self):
         print("Finalizing content")
 
