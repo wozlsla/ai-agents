99610c40dd64f094cbd878a1c4bae7afcbafc837
jimin <jiminseo0307@gmail.com>
2025-09-21 18:41:46 +0900

    feat(email-optimizer-agent): 6.1 email optimization agent team with Autogen RoundRobinGroupChat

---
+++ deep-research-clone/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from deep-research-clone!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ deep-research-clone/tools.py
@@ -0,0 +1,61 @@
+import os, re, requests
+
+
+def web_search_tool(query: str) -> str:
+    """
+    Web Search Tool. (Firecrawl Code Snippets)
+    Args:
+        query: str
+            The query to search the web for.
+    Returns
+        A list of search results with the website content in Markdown format.
+    """
+    url = "https://api.firecrawl.dev/v2/search"
+
+    payload = {
+        "query": query,
+        "sources": ["web"],
+        "categories": [],
+        "limit": 5,
+        "scrapeOptions": {
+            "onlyMainContent": True,
+            "maxAge": 172800000,
+            # "parsers": ["pdf"],
+            "formats": ["markdown"],
+        },
+    }
+
+    headers = {
+        "Authorization": f"Bearer {os.getenv('FIRECRAWL_API_KEY')}",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    response = response.json()
+
+    # 후처리
+    if response["success"] is not True:
+        return "Error using tool."
+
+    cleaned_chunks = []
+
+    for result in response["data"]["web"]:
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"] if "markdown" in result else ""
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_result = {
+            "title": title,
+            "url": url,
+            "markdown": cleaned,
+        }
+
+        cleaned_chunks.append(cleaned_result)
+
+    return cleaned_chunks
+
+
+# print(web_search_tool("ai engineer jobs in south korea"))
