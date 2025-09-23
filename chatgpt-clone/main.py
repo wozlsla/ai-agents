d1582446514406d0fa8c9eda847d188d616886c2
jimin <jiminseo0307@gmail.com>
2025-09-23 15:40:34 +0900

    feat(chatgpt-clone): 8.2 web search tool

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -4,7 +4,7 @@ dotenv.load_dotenv()
 
 import asyncio
 import streamlit as st
-from agents import Agent, Runner, SQLiteSession
+from agents import Agent, Runner, SQLiteSession, WebSearchTool
 
 
 # Agent
@@ -13,7 +13,11 @@ if "agent" not in st.session_state:
         name="ChatGPT Clone",
         instructions="""
         You are a helpful assistant.
+ 
+        You have access to the followig tools:
+            - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
         """,
+        tools=[WebSearchTool()],
     )
 
 agent = st.session_state["agent"]
@@ -33,25 +37,58 @@ async def paint_history():
     messages = await session.get_items()
 
     for message in messages:
-        with st.chat_message(message["role"]):
-            if message["role"] == "user":
-                st.write(message["content"])
-            elif message["type"] == "message":
-                st.write(message["content"][0]["text"])
+
+        if "role" in message:
+            with st.chat_message(message["role"]):
+                if message["role"] == "user":
+                    st.write(message["content"])
+                elif message["type"] == "message":
+                    st.write(message["content"][0]["text"])
+
+        if "type" in message and message["type"] == "web_search_call":
+            with st.chat_message("ai"):
+                st.write("Searching Web...")
 
 
+def update_status(status_container, event):
+
+    # just for web search tool
+    status_messages = {
+        "response.web_search_call.completed": ("✅ Web search completed.", "complete"),
+        "response.web_search_call.in_progress": (
+            "🔍 Starting web search...",
+            "running",
+        ),
+        "response.web_search_call.searching": (
+            "🔍 Web search in progress...",
+            "running",
+        ),
+        "response.completed": (" ", "complete"),
+    }
+
+    if event in status_messages:
+        label, state = status_messages[event]
+        status_container.update(label=label, state=state)
+
+
+# Draw UI
 asyncio.run(paint_history())
 
 
 # Runner
 async def run_agent(message):
     with st.chat_message("ai"):
+        status_container = st.status("⏳", expanded=False)
         text_placeholder = st.empty()
         response = ""
+
         stream = Runner.run_streamed(agent, message, session=session)
 
         async for event in stream.stream_events():
             if event.type == "raw_response_event":
+
+                update_status(status_container, event.data.type)
+
                 if event.data.type == "response.output_text.delta":
                     response += event.data.delta
                     text_placeholder.write(response)
