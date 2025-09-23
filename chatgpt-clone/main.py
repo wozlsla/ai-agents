fdcd77926f647954edcac4a0b672661f0b50606d
jimin <jiminseo0307@gmail.com>
2025-09-23 12:44:12 +0900

    feat(chatgpt-clone): 8.1 chat UI and conversation history
    remove dummy-agent .db

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,6 +1,76 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import dotenv
+
+dotenv.load_dotenv()
+
+import asyncio
+import streamlit as st
+from agents import Agent, Runner, SQLiteSession
 
 
-if __name__ == "__main__":
-    main()
+# Agent
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+
+agent = st.session_state["agent"]
+
+
+# Session 초기화
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",  # ession id
+        "chat-gpt-clone-memory.db",  # .db path
+    )
+
+session = st.session_state["session"]
+
+
+async def paint_history():
+    messages = await session.get_items()
+
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            elif message["type"] == "message":
+                st.write(message["content"][0]["text"])
+
+
+asyncio.run(paint_history())
+
+
+# Runner
+async def run_agent(message):
+    with st.chat_message("ai"):
+        text_placeholder = st.empty()
+        response = ""
+        stream = Runner.run_streamed(agent, message, session=session)
+
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                    response += event.data.delta
+                    text_placeholder.write(response)
+
+
+### UI ###
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+
+    asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+
+    reset = st.button("Reset Memory")
+    if reset:
+        asyncio.run(session.clear_session())
+
+    st.write(asyncio.run(session.get_items()))
