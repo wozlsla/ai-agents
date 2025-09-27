150b6098b72a8372da4da8538823006fb3ce58e3
jimin <jiminseo0307@gmail.com>
2025-09-27 15:32:06 +0900

    fix(customer-support-agent): 9.5 persist agent state after handoff

--- customer-support-agent/main.py
+++ customer-support-agent/main.py
@@ -24,6 +24,9 @@ if "session" not in st.session_state:
     )
 session = st.session_state["session"]
 
+if "agent" not in st.session_state:
+    st.session_state["agent"] = triage_agent
+
 
 async def paint_history():
     messages = await session.get_items()
@@ -50,7 +53,7 @@ async def run_agent(message):
 
         try:
             stream = Runner.run_streamed(
-                triage_agent,
+                st.session_state["agent"],
                 message,
                 session=session,
                 context=user_account_ctx,
@@ -62,6 +65,20 @@ async def run_agent(message):
                     if event.data.type == "response.output_text.delta":
                         response += event.data.delta
                         text_placeholder.write(response.replace("$", "\\$"))
+
+                # update current agent to transferred agent
+                elif event.type == "agent_updated_stream_event":
+
+                    if st.session_state["agent"].name != event.new_agent.name:
+                        st.write(
+                            f"🤖 transferred from {st.session_state["agent"].name} to {event.new_agent.name}"
+                        )
+
+                        st.session_state["agent"] = event.new_agent
+
+                        text_placeholder = st.empty()
+                        st.session_state["text_placeholder"] = text_placeholder
+                        response = ""
 
         except InputGuardrailTripwireTriggered:
             st.write("I can't help you with that.")
