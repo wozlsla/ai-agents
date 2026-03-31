fbda1e10c6fb8c37f9bc4d6b847a1c5b2ab353d5
jimin <jiminseo0307@gmail.com>
2026-04-01 00:25:48 +0900

    feat(workflow-testing): 17.1 init email graph, no llm

---
+++ workflow-testing/main.py
@@ -0,0 +1,69 @@
+from typing import Literal, List
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+
+
+class EmailState(TypedDict):
+
+    email: str
+    category: Literal["spam", "normal", "urgent"]
+    priority_score: int
+    response: str
+
+
+def categorize_email(state: EmailState):
+    email = state["email"].lower()
+
+    if "urgent" in email or "asap" in email:
+        category = "urgent"
+    elif "offer" in email or "discount" in email:
+        category = "spam"
+    else:
+        category = "normal"
+
+    return {
+        "category": category,
+    }
+
+
+def assign_priority(state: EmailState):
+    scores = {
+        "urgent": 10,
+        "normal": 5,
+        "spam": 1,
+    }
+    return {
+        "priority_score": scores[state["category"]],
+    }
+
+
+def draft_response(state: EmailState) -> EmailState:
+    responses = {
+        "urgent": "I will answer you as fast as i can",
+        "normal": "I'll get back to you soon",
+        "spam": "Go away!",
+    }
+    return {
+        "response": responses[state["category"]],
+    }
+
+
+graph_builder = StateGraph(EmailState)
+
+
+graph_builder.add_node("categorize_email", categorize_email)
+graph_builder.add_node("assign_priority", assign_priority)
+graph_builder.add_node("draft_response", draft_response)
+
+graph_builder.add_edge(START, "categorize_email")
+graph_builder.add_edge("categorize_email", "assign_priority")
+graph_builder.add_edge("assign_priority", "draft_response")
+graph_builder.add_edge("draft_response", END)
+
+graph = graph_builder.compile()
+
+
+result = graph.invoke({"email": "I have an offer for you!"})
+
+print(result)
